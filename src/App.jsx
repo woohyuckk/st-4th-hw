e2844c7629293e9be178e371e8105cc73480c7ec
woohyuckk <woohyuckk@gmail.com>
2025-02-02 16:45:42 +0900

    refact : /detail 경로 id 1번으로 표시

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -21,7 +21,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/detail" element={<Detail />} >
+          <Route index element={<Navigate to="1" replace />} />
+          <Route path=":id" element={<Detail/>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
